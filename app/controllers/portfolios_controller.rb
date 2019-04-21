24e3e01a43c6e1041b36f11c42a62c9b0a7dfc3a
MikeWick <wickert.michael@gmail.com>
2019-04-21 05:51:02 -0400

    Implemented collection based partials

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -19,7 +19,7 @@ class PortfoliosController < ApplicationController
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Blog was successfully created.' }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
