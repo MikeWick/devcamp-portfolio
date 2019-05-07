a2d597c8727014b220363965f6edd60c307e2765
MikeWick <wickert.michael@gmail.com>
2019-05-07 00:08:08 -0400

    Updated file upload forms and refactored placeholder functionality into view

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,23 @@
 module PortfoliosHelper
+  def image_generator(height:, width:)
+    "https://via.placeholder.com/#{height}x#{width}"
+  end
+  
+  def portfolio_img img, type
+    if type == 'thumb'
+      img.thumb_image.url || image_generator(height: '350', width:'200')
+    else
+      img.main_image.url || image_generator(height: '600', width: '400')
+    end
+    
+#    if img
+#      img
+#    elsif type == 'thumb'
+#      image_generator(height: '350', width: '200')
+#    else
+#      image_generator(height: '600', width: '400')
+#    end
+  end
 end
+
+
