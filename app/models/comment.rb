8fff06ff29770346c775ac773dbd7e9b7c299254
MikeWick <wickert.michael@gmail.com>
2019-05-07 23:57:12 -0400

    Completed comment feature with action cable, and prepended comments so the newest comments are posted at the top

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,8 @@ class Comment < ApplicationRecord
   belongs_to :blog
   
   validates :content, presence: true, length: { minimum: 5, maximum: 1000 }
+  default_scope -> {order("created_at DESC")}
   
   after_create_commit { CommentBroadcastJob.perform_later(self) }
+  
 end
